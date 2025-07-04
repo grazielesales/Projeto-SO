48ef42beb9b5e0f5f365f3b3fafcf6022fa91d75
Graziele Sales <99915339+grazielesales@users.noreply.github.com>
2025-07-04 19:56:25 -0300

    Desativa preempção no modo FCFS para evitar deadlock
    Ajuste no problema de lógica

--- minix/kernel/proc.c
+++ minix/kernel/proc.c
@@ -1604,63 +1604,72 @@ void enqueue(
  * This function can be used x-cpu as it always uses the queues of the cpu the
  * process is assigned to.
  */
+ 
+	/* MODIFICAÇÃO 1: Declaração da variável 'q' no início */
 	int q;
-#if FCFS_MODE
-    q = 7;  // Manda TODOS os processos para a fila 7 (USER_Q)
+
+	/* MODIFICAÇÃO 2: Lógica para definir a fila 'q' baseada no modo FCFS */
+#ifdef FCFS_MODE
+	/* Para FCFS, todos os processos de usuário vão para a mesma fila. */
+	q = USER_Q;
 #else
-    q = rp->p_priority; 
+	/* Para o modo padrão, a fila é a prioridade do processo. */
+	q = rp->p_priority;
 #endif
-	
-  struct proc **rdy_head, **rdy_tail;
-  
-  assert(proc_is_runnable(rp));
 
-  assert(q >= 0 && q < NR_SCHED_QUEUES);
+	struct proc **rdy_head, **rdy_tail;
 
-  rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
-  rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
+	assert(proc_is_runnable(rp));
+	assert(q >= 0 && q < NR_SCHED_QUEUES);
 
-  /* Now add the process to the queue. */
-  if (!rdy_head[q]) {		/* add to empty queue */
-      rdy_head[q] = rdy_tail[q] = rp; 		/* create a new queue */
-      rp->p_nextready = NULL;		/* mark new end */
-  } 
-  else {					/* add to tail of queue */
-      rdy_tail[q]->p_nextready = rp;		/* chain tail of queue */	
-      rdy_tail[q] = rp;				/* set new queue tail */
-      rp->p_nextready = NULL;		/* mark new end */
-  }
+	rdy_head = get_cpu_var(rp->p_cpu, run_q_head);
+	rdy_tail = get_cpu_var(rp->p_cpu, run_q_tail);
 
-  if (cpuid == rp->p_cpu) {
-	  /*
-	   * enqueueing a process with a higher priority than the current one,
-	   * it gets preempted. The current process must be preemptible. Testing
-	   * the priority also makes sure that a process does not preempt itself
-	   */
-	  struct proc * p;
-	  p = get_cpulocal_var(proc_ptr);
-	  assert(p);
-	  if((p->p_priority > rp->p_priority) &&
-			  (priv(p)->s_flags & PREEMPTIBLE))
-		  RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
-  }
+	/* Now add the process to the queue. */
+	if (!rdy_head[q]) {		/* add to empty queue */
+		rdy_head[q] = rdy_tail[q] = rp; 		/* create a new queue */
+		rp->p_nextready = NULL;		/* mark new end */
+	} 
+	else {					/* add to tail of queue */
+		rdy_tail[q]->p_nextready = rp;		/* chain tail of queue */	
+		rdy_tail[q] = rp;				/* set new queue tail */
+		rp->p_nextready = NULL;		/* mark new end */
+	}
+
+	/* MODIFICAÇÃO 3: Desativa o bloco de preempção se FCFS_MODE estiver ativo */
+#ifndef FCFS_MODE
+	if (cpuid == rp->p_cpu) {
+		/*
+		 * enqueueing a process with a higher priority than the current one,
+		 * it gets preempted. The current process must be preemptible. Testing
+		 * the priority also makes sure that a process does not preempt itself
+		 */
+		struct proc * p;
+		p = get_cpulocal_var(proc_ptr);
+		assert(p);
+		if((p->p_priority > rp->p_priority) &&
+			(priv(p)->s_flags & PREEMPTIBLE))
+			RTS_SET(p, RTS_PREEMPTED); /* calls dequeue() */
+	}
+#endif /* FCFS_MODE */
+
 #ifdef CONFIG_SMP
-  /*
-   * if the process was enqueued on a different cpu and the cpu is idle, i.e.
-   * the time is off, we need to wake up that cpu and let it schedule this new
-   * process
-   */
-  else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
-	  smp_schedule(rp->p_cpu);
-  }
+	/*
+	 * if the process was enqueued on a different cpu and the cpu is idle, i.e.
+	 * the time is off, we need to wake up that cpu and let it schedule this new
+	 * process
+	 */
+	else if (get_cpu_var(rp->p_cpu, cpu_is_idle)) {
+		smp_schedule(rp->p_cpu);
+	}
 #endif
 
-  /* Make note of when this process was added to queue */
-  read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
+	/* Make note of when this process was added to queue */
+	read_tsc_64(&(get_cpulocal_var(proc_ptr)->p_accounting.enter_queue));
 
 
 #if DEBUG_SANITYCHECKS
-  assert(runqueues_ok_local());
+	assert(runqueues_ok_local());
 #endif
 }
 
