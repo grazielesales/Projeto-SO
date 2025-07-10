828d4dd0c8815adb01bb8d5710dfc2d5cfcb6031
ThiagoRberto <68163197+ThiagoRberto@users.noreply.github.com>
2025-07-09 21:08:33 -0300

    Fixing System Panic

--- minix/kernel/proc.c
+++ minix/kernel/proc.c
@@ -1798,7 +1798,7 @@ static struct proc * pick_proc(void)
     rdy_head = get_cpulocal_var(run_q_head);
 
     /* 1) Somar tickets de todos os processos prontos */
-    for (rp = BEG_PROC_ADDR; rp <= END_PROC_ADDR; rp++) {
+    for (rp = BEG_PROC_ADDR; rp < END_PROC_ADDR; rp++) {
         if (proc_is_runnable(rp)) {
             total += rp->tickets;
         }
@@ -1812,7 +1812,7 @@ static struct proc * pick_proc(void)
 
 
         /* 3) Achar o processo vencedor */
-        for (rp = BEG_PROC_ADDR; rp <= END_PROC_ADDR; rp++) {
+        for (rp = BEG_PROC_ADDR; rp < END_PROC_ADDR; rp++) {
             if (proc_is_runnable(rp)) {
                 acc += rp->tickets;
                 if (acc >= winner) {
